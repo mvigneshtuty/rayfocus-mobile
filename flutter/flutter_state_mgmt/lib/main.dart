7ca79ce3d4e996181312911939dc61b9cbeff67e
Vignesh M <mvigneshtuty@gmail.com>
2018-08-03 11:43:26 +0530

    addition of different state mgmt. techniques

--- flutter/flutter_state_mgmt/lib/main.dart
+++ flutter/flutter_state_mgmt/lib/main.dart
@@ -19,9 +19,9 @@ class FlutterStateMgmtApp extends StatelessWidget {
           children: <Widget>[
             generateTapBoxWidgetSection(TapBoxAwidget(),"Widget manages it's own state"),
             Divider(),
-            generateTapBoxWidgetSection(ParentWidget(),"Parent Widget manages the state of child widget"),
+            generateTapBoxWidgetSection(ParentBwidget(),"Parent widget manages the state of child widget"),
             Divider(),
-            generateTapBoxWidgetSection(TapBoxAwidget(),"Yet another Widget manages it's own state")
+            generateTapBoxWidgetSection(ParentCwidget(),"State managed by both parent and child widgets")
           ],
         )
       ),
@@ -58,6 +58,7 @@ Widget generateTapBoxWidgetSection(Widget tapBoxWidget, String tapBoxLabel){
 }
 
 // TapBox A => the widget handles its own state
+//*********************************************
 class TapBoxAwidget extends StatefulWidget{
   @override
   State<StatefulWidget> createState() {
@@ -86,7 +87,7 @@ class TapBoxAwidgetState extends State{
         width: 100.0,
         height: 100.0,
         decoration: BoxDecoration(
-          color: boxAstate ? Colors.green : Colors.grey
+          color: boxAstate ? Colors.green : Colors.grey[500]
         ),
       ),
     );
@@ -94,6 +95,35 @@ class TapBoxAwidgetState extends State{
 }
 
 // TapBox B => the state is managed by parent widget
+//**************************************************
+
+// Parent widget to control the TabBox B state
+class ParentBwidget extends StatefulWidget {
+  @override
+  _ParentBwidgetState createState() => _ParentBwidgetState();
+}
+
+class _ParentBwidgetState extends State<ParentBwidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapBoxBwidget(
+        boxBstate: _active,
+        onTapBoxBstateChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+// Child widget => TapBoxBwidget
 class TapBoxBwidget extends StatelessWidget{
 
   final bool boxBstate;
@@ -119,23 +149,28 @@ class TapBoxBwidget extends StatelessWidget{
         width: 100.0,
         height: 100.0,
         decoration: BoxDecoration(
-            color: boxBstate ? Colors.green : Colors.yellow
+            color: boxBstate ? Colors.green : Colors.grey[500]
         ),
       ),
     );
   }
 }
 
-// Parent widget to control the TabBox B state
-class ParentWidget extends StatefulWidget {
+
+
+// TapBox C => the state is managed by both parent and child widget
+//*****************************************************************
+
+class ParentCwidget extends StatefulWidget{
+
   @override
-  _ParentWidgetState createState() => _ParentWidgetState();
+  State<StatefulWidget> createState() => ParentCwidgetState();
 }
 
-class _ParentWidgetState extends State<ParentWidget> {
+class ParentCwidgetState extends State<ParentCwidget>{
   bool _active = false;
 
-  void _handleTapboxChanged(bool newValue) {
+  void _handleTapboxChanged(bool newValue){
     setState(() {
       _active = newValue;
     });
@@ -144,9 +179,78 @@ class _ParentWidgetState extends State<ParentWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TapBoxBwidget(
-        boxBstate: _active,
-        onTapBoxBstateChanged: _handleTapboxChanged,
+      child: TapBoxCwidget(
+        boxCstate: _active,
+        onTapBoxCstateChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+// Child widget => TapBoxCwidget
+
+class TapBoxCwidget extends StatefulWidget{
+
+  bool boxCstate = false;
+  final ValueChanged<bool> onTapBoxCstateChanged;
+
+  TapBoxCwidget({
+    this.boxCstate: false,
+    @required this.onTapBoxCstateChanged
+  });
+
+  @override
+  State<StatefulWidget> createState() => TapBoxCwidgetState();
+}
+
+// Child widget state => TapBoxCwidget state
+class TapBoxCwidgetState extends State<TapBoxCwidget>{
+
+  bool _highlightBorder = false;
+
+  _handleTapDown(TapDownDetails details){
+    setState(() {
+      _highlightBorder = true;
+    });
+  }
+
+  _handleTapUp(TapUpDetails details){
+    setState(() {
+      _highlightBorder = false;
+    });
+  }
+
+  _handleTap(){
+    widget.onTapBoxCstateChanged(!widget.boxCstate);
+  }
+
+  _handleTapCancel(){
+    setState(() {
+      _highlightBorder = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: Container(
+        child: Center(
+          child: Text(
+              widget.boxCstate ? 'Active' : 'InActive'
+          ),
+        ),
+        width: 100.0,
+        height: 100.0,
+        decoration: BoxDecoration(
+            color: widget.boxCstate ? Colors.green : Colors.grey[500],
+            border: _highlightBorder
+                    ? Border.all( color: Colors.red, width: 10.0)
+                    : null
+        ),
       ),
     );
   }
